3639c9c04acd77452c0759606f37f1b48b36f7d0
caoyucong <caoyucong1@zto.com>
2021-03-01 11:07:00 +0800

    feat: Access-Control-Allow-Headers 添加 Authorization

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app
     // }
     ctx.set('Access-Control-Allow-Origin', '*')
 
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
     ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
 
